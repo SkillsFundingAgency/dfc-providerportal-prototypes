f1bebeea4eb801d3b0187fcfeb9f80c3c3a38cdd
Westley Knight <westley.knight@gmail.com>
2020-10-06 18:01:08 +0100

    Progress on adding T levels

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,21 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// GET SPRINT NAME - useful for relative templates
+router.use('/', (req, res, next) => {
+    res.locals.currentURL = req.originalUrl; //current screen
+    res.locals.prevURL = req.get('Referrer'); // previous screen
+    console.log('previous page is: ' + res.locals.prevURL + " and current page is " + req.url + " " + res.locals.currentURL );
+    next();
+});
+
+
+router.post('/sprint-13/tlevels-add', function (req, res) {
+    res.redirect('/sprint-13/tlevels-description')
+})
+
 module.exports = router
+
+//module.exports = function (router) {
+
+//}
