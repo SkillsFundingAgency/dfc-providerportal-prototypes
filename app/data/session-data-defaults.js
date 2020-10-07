ea6fd1bfca2f8243d298f54ca7cd454fce83e38a
cacour10 <60186633+cacour10@users.noreply.github.com>
2020-10-07 13:17:55 +0100

    Exemplar data  - Test1

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -23,7 +23,9 @@ module.exports = {
   "sprint13-tlevels-count": 0,
 
   // Exemplar content for Design, Surveying and Planning for Construction T Level
-  "tlevels-parent-design-whothiscourseisfor": "",
+  "tlevels-parent-design-whothiscourseisfor": "This course is suitable for anyone wanting a career in construction, specifically in surveying and design, civil engineering, building services design, or hazardous materials surveying.
+
+During the two-year programme, you will learn the core knowledge and skills that underpin many jobs in the construction industry. Your learning will combine 40 weeks of classroom theory and practical learning and a minimum of 9 weeks with an employer.",
   "tlevels-parent-design-entryrequirements": "",
   "tlevels-parent-design-whatyoulllearn": "",
   "tlevels-parent-design-howyoulllearn": "",
