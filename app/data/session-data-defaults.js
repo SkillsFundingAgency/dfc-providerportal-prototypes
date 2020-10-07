bc21f686f6a7c0d2b1968cdb8ef77585139da236
Westley Knight <westley.knight@gmail.com>
2020-10-07 13:06:43 +0100

    Additiona of variables for exemplat data for T Levels

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -20,6 +20,30 @@ Example usage:
 module.exports = {
 
   // Insert values here
-   "sprint13-tlevels-count": 0
+  "sprint13-tlevels-count": 0,
+
+  // Exemplar content for Design, Surveying and Planning for Construction T Level
+  "tlevels-parent-design-whothiscourseisfor": "",
+  "tlevels-parent-design-entryrequirements": "",
+  "tlevels-parent-design-whatyoulllearn": "",
+  "tlevels-parent-design-howyoulllearn": "",
+  "tlevels-parent-design-howassessed": "",
+  "tlevels-parent-design-whatyoucandonext": "",
+
+ // Exemplar content for Digital Production, Design and Development
+  "tlevels-parent-digital-whothiscourseisfor": "",
+  "tlevels-parent-digital-entryrequirements": "",
+  "tlevels-parent-digital-whatyoulllearn": "",
+  "tlevels-parent-digital-howyoulllearn": "",
+  "tlevels-parent-digital-howassessed": "",
+  "tlevels-parent-digital-whatyoucandonext": "",
+
+  // Exemplar content for Education and Childcare T Level
+  "tlevels-parent-education-whothiscourseisfor": "",
+  "tlevels-parent-education-entryrequirements": "",
+  "tlevels-parent-education-whatyoulllearn": "",
+  "tlevels-parent-education-howyoulllearn": "",
+  "tlevels-parent-education-howassessed": "",
+  "tlevels-parent-education-whatyoucandonext": ""
 
 }
