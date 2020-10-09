250305df8dc8484b8dfd669ad96c8a3b9462e1f5
cacour10 <60186633+cacour10@users.noreply.github.com>
2020-10-09 09:41:30 +0100

    T Level Construction exemplar data

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -24,11 +24,11 @@ module.exports = {
 
   // Exemplar content for Design, Surveying and Planning for Construction T Level
   "tlevels-parent-design-whothiscourseisfor": "This course is suitable for anyone wanting a career in construction, specifically in surveying and design, civil engineering, building services design, or hazardous materials surveying.\n\nDuring the two-year programme, you will learn the core knowledge and skills that underpin many jobs in the construction industry. Your learning will combine 40 weeks of classroom theory and practical learning and a minimum of 9 weeks with an employer.",
-  "tlevels-parent-design-entryrequirements": "",
-  "tlevels-parent-design-whatyoulllearn": "",
-  "tlevels-parent-design-howyoulllearn": "",
-  "tlevels-parent-design-howassessed": "",
-  "tlevels-parent-design-whatyoucandonext": "",
+  "tlevels-parent-design-entryrequirements": "You will need either, 5 GCSE passes (grade 4 or above), including English, maths and science or a pass in a relevant level 2 qualification.",
+  "tlevels-parent-design-whatyoulllearn": "You'll learn specific topics in design, surveying and planning:\n\nProject management\nBudgeting and resource allocation\nProcurement\nRisk management\n\nIn addition to the core content, you will have the option to study one of the following specialisms:\n\nSurveying and design for construction and the built environment\nCivil engineering\nBuilding services design\nHazardous materials analysis and surveying",
+  "tlevels-parent-design-howyoulllearn": "Your learning will combine 40 weeks of classroom theory and practical learning and include a minimum of 9 weeks with an employer. The industry placement will provide you with a real experience of the workplace.\n\nYou will take part in investigative and practical work and during the T Level you will be expected to do private study and e-learning.",
+  "tlevels-parent-design-howassessed": "A written examination will take place at the end of the T Level and task based assessments will take place throughout the T Level.",
+  "tlevels-parent-design-whatyoucandonext": "This T Level has been developed in collaboration with employers and businesses so the content will meet the needs of the industry and prepare you for work, providing you the knowledge and experience needed to open the door to highly skilled employment, an Apprenticeship or higher-level study.\n\nYou can progress into roles such as:\nCivil engineering technician\nTechnical surveyor\nBuilding technician\nEngineering construction technician\nArchitectural technician",
 
  // Exemplar content for Digital Production, Design and Development
   "tlevels-parent-digital-whothiscourseisfor": "",
