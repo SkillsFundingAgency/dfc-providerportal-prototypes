f7bf950eb7ccd670806029adbf21ae50a4301954
Westley Knight <westley.knight@gmail.com>
2020-11-23 10:59:07 +0000

    Refinement of editing T Levels

--- app/routes/sprint-16.js
+++ app/routes/sprint-16.js
@@ -5,26 +5,41 @@ module.exports = function (router) {
         'June', 'July', 'August', 'September',
         'October', 'November', 'December'
         ];
+
+    var shortmonths = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May',
+        'Jun', 'Jul', 'Aug', 'Sep',
+        'Oct', 'Nov', 'Dec'
+        ];
     
     function monthNumToName(monthnum) {
         return months[monthnum - 1] || '';
     }
 
+    function monthNumToShortName(monthnum) {
+        return shortmonths[monthnum - 1] || '';
+    }
 
     // Set variables for T Level locations
 
 
-        router.get('/sprint-16/tlevels-view', function (req, res) {
-            if (!req.session.data['tlevels-details-location']){
-                req.session.data['tlevels-details-location'] = ["Avenue Campus","Medway Campus"]
+        router.get('/sprint-16/tlevels-setvars', function (req, res) {
+            if (!req.session.data['s16-tlevels-details-location']){
+                req.session.data['s16-tlevels-details-location'] = ["Avenue Campus","Medway Campus"]
+                req.session.data["s16-tlevels-desc-whothiscourseisfor"] = "This T Level is suitable for anyone wanting a career in construction, specifically in surveying and design, civil engineering, building services design, or hazardous materials surveying. During the two-year programme, you will learn the core knowledge and skills that underpin many jobs in the construction industry. Your learning will combine 40 weeks of classroom theory and practical learning and a minimum of 9 weeks with an employer."
+                req.session.data["s16-tlevels-desc-entryrequirements"] = "You will need either, 5 GCSE passes (grade 4 or above), including English, maths and science or a pass in a relevant level 2 qualification."
+                req.session.data["s16-tlevels-desc-whatyoulllearn"] = "You'll learn specific topics in design, surveying and planning: Project management, Budgeting and resource allocation, Procurement, Risk management. In addition to the core content, you will have the option to study one of the following specialisms: Surveying and design for construction and the built environment, Civil engineering, Building services design, Hazardous materials analysis and surveying."
+                req.session.data["s16-tlevels-desc-howyoulllearn"] = "Your learning will combine 40 weeks of classroom theory and practical learning and include a minimum of 9 weeks with an employer. The industry placement will provide you with a real experience of the workplace. You will take part in investigative and practical work and during the T Level you will be expected to do private study and e-learning."
+                req.session.data["s16-tlevels-desc-howassessed"] = "A written examination will take place at the end of the T Level and task based assessments will take place throughout the T Level."
+                req.session.data["s16-tlevels-desc-whatyoucandonext"] = "This T Level has been developed in collaboration with employers and businesses so the content will meet the needs of the industry and prepare you for work, providing you the knowledge and experience needed to open the door to highly skilled employment, an Apprenticeship or higher-level study. You can progress into roles such as: Civil engineering technician, Technical surveyor, Building technician, Engineering construction technician, Architectural technician."
             }
-            res.render('sprint-16/tlevels-view');
+            res.redirect('/sprint-16/tlevels-list');
         })
 
-        router.post('/sprint-16/tlevels-edit', function (req, res) {
-            res.redirect('/sprint-16/tlevels-edit-checkanswers');
+        router.post('/sprint-16/tlevels-edit-details', function (req, res) {
+            req.session.data["s16-tlevels-details-startdate-monthname"] = monthNumToShortName(req.session.data["s16-tlevels-details-startdate-month"]);
+            res.redirect('/sprint-16/tlevels-view-success');
         })
-
 
 
     // Nested checkbox journey for provider type
