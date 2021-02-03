05f1a520b41ac050dee6bc94e700daaae7a3cfef
Westley Knight <westley.knight@gmail.com>
2021-02-03 16:14:35 +0000

    Addition of updated location journeys for Add and Delete

---
+++ app/routes/sprint-20.js
@@ -0,0 +1,35 @@
+module.exports = function (router) {
+
+    var v = "sprint-20"
+
+    // Locations - Add
+
+        router.post('/'+v+'/venues/add/select', function (req, res) {
+            res.redirect('/'+v+'/venues/add/details');
+        })
+
+        router.post('/'+v+'/venues/add/manual', function (req, res) {
+            res.redirect('/'+v+'/venues/add/details');
+        })
+
+        router.post('/'+v+'/venues/add/details', function (req, res) {
+            res.redirect('/'+v+'/venues/add/check-publish');
+        })
+
+        router.post('/'+v+'/venues/add/check-publish', function (req, res) {
+            res.redirect('/'+v+'/venues/add/success');
+        })
+
+        
+    // Locations - Delete
+    
+        router.post('/'+v+'/venues/delete', function (req, res) {
+            if (req.session.data['location-delete-sure'] == "No, go back"){
+                res.redirect('/'+v+'/venues/index');
+            } else {
+                res.redirect('/'+v+'/venues/delete/success');
+            }
+        })
+
+
+}
